0d0360e757d77c742c575a9a5587086e92db8975
orLincoln <orleandroat@gmail.com>
2026-05-18 15:24:19 -0300

    upando-projeto

---
+++ src/main/java/com/pessoal/organizador/OrganizadorApplication.java
@@ -0,0 +1,13 @@
+package com.pessoal.organizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pessoal/organizador/OrganizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pessoal.organizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
